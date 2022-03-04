ad3b9087d211e1d63ce2f5f14ad439ead7106193
umar-khayam <u.khayam03@gmail.com>
2022-03-04 22:16:52 +0500

    Do some practice on loop, functions.

---
+++ Practice/Practice of Syntax.js
@@ -0,0 +1,22 @@
+//console.log('Hello World')
+//document.getElementById('TextChange').innerHTML="New World"
+
+//When we declare variable with let, we can't create that name variable next
+
+/*
+Null vs Undifined
+The undefined value is a primitive value used when a variable has 
+not been assigned a value. The null value is a primitive value that 
+represents the null, empty, or non-existent reference*/
+
+//Shallow copy
+//Shallow copy point to the same location where memory point same as reference type
+
+//Deep copy
+//Deep copy point to the new location same as value type
+
+//Now we moved on functions:
+function num(){
+    return 3
+}
+console.log(num())
