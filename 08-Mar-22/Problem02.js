8ccd713baa9242abfcd0db69d44a1dca3e92a15c
umar-khayam <u.khayam03@gmail.com>
2022-03-08 22:54:02 +0500

    Quiz problems updated

--- 08-Mar-22/Problem02.js
+++ 08-Mar-22/Problem02.js
@@ -2,6 +2,7 @@ var student = {
     name : "Umar Khayam",
     sclass : "VI",
     rollno : 65 };
+    
 console.log('Before Deleting: '+student.rollno)
 delete student.rollno;
 console.log('After Deleting: '+student.rollno)
