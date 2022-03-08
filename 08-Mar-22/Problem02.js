4f32a7b7863f53af4e329be572f9a3dc12fde486
umar-khayam <u.khayam03@gmail.com>
2022-03-08 22:54:28 +0500

    Quiz's problem completed

--- 08-Mar-22/Problem02.js
+++ 08-Mar-22/Problem02.js
@@ -2,7 +2,6 @@ var student = {
     name : "Umar Khayam",
     sclass : "VI",
     rollno : 65 };
-    
 console.log('Before Deleting: '+student.rollno)
 delete student.rollno;
 console.log('After Deleting: '+student.rollno)
