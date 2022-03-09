99eeefe24a3cb779affebdafe3112eef2da825fe
umar-khayam <u.khayam03@gmail.com>
2022-03-09 23:34:14 +0500

    09-Mar-2022 Quiz's practice

---
+++ Practice/todayQuiz.js
@@ -0,0 +1,29 @@
+class Student{
+    constructor (Name){
+        this.Name=Name;
+        this.gpa;
+        this.percentage;
+        
+    }
+    set Gpa(gpa){
+        // if(gpa >=0 || gpa<=4)
+        // {
+        this.gpa=gpa;
+        // }
+    }
+    get percentage(){
+        return ((this.gpa*4)/100);
+    }
+    get Details(){
+        var obj={Name: this.Name, Gpa:this.gpa, Percentage:this.percentage};
+        return obj;
+        // return [this.Name,this.gpa,this.percentage];
+    }
+}
+
+var obj=new Student("Haseeb");
+obj.Gpa=3;
+// var a=obj.Details();
+let {Name,Gpa,Percentage}=obj.Details;
+console.log(Name,Gpa, Percentage);
+// console.log(a);
