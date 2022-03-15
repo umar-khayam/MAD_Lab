107b38412a6e4d2bbac5c0cee9ee25a7d2a1f883
umar-khayam <u.khayam03@gmail.com>
2022-03-15 19:07:27 +0500

    Updated

--- Assignment/14-Mar-22/script.js
+++ Assignment/14-Mar-22/script.js
@@ -21,9 +21,11 @@ function add_list() {
   const fname=document.getElementById('fname');
   const data = fname.value;
   const v=document.createElement("li");
+  v.className='list_items'
   v.innerText=data;
   v.appendChild(checkbox_create())
   list.appendChild(v)
+  console.log(v)
 }
 
 function checkbox_create() {
@@ -33,5 +35,17 @@ function checkbox_create() {
 }
 
 function removeTodo() {  
-
+  const val = document.querySelectorAll('.list_items');
+  var arr=Array.from(val)
+  let count = 0;
+    
+  arr.forEach(
+    (item)=>{
+    count+=1;
+      if(count == arr.length)
+    {
+    list.removeChild(item);
+    }
+  })
 }
+
